59a9c8dff20bcf7de26483c613fd197cd6ff70ab
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-01-29 23:16:27 -0800

    adding missing termite directory.

--- manifests/terminal.pp
+++ manifests/terminal.pp
@@ -1,6 +1,11 @@
  class ytlaces::terminal{
    package {"termite":}
 
+   file {"/home/tsutsumi/.config/termite":
+    ensure => 'directory',
+    owner => "tsutsumi"
+   }
+
    file {"/home/tsutsumi/.config/termite/config":
     ensure => 'file',
     source => "puppet:///modules/ytlaces/.config/termite/config",
