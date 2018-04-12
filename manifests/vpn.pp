acd79703a7ac289143f48c3e7e516a6589effa7d
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-04-11 22:32:12 -0700

    adding provisioning for vpn coming back on laptop shutdown / suspend.

--- manifests/vpn.pp
+++ manifests/vpn.pp
@@ -1,4 +1,48 @@
 class ytlaces::vpn {
   # enables the international characters (e.g. japanese)
   package {"openconnect":}
+  file {"/etc/NetworkManager/dispatcher.d/handle_vpn":
+   ensure => "file",
+   source => "puppet:///modules/ytlaces/etc/NetworkManager/dispatcher.d/handle_vpn",
+   owner => "root",
+   mode => "0755"
+  }
+
+  file {"/etc/vpnc/connect.d/":
+   ensure => 'directory',
+  }
+
+  file {"/etc/vpnc/connect.d/write_gateway_file":
+   ensure => "file",
+   source => "puppet:///modules/ytlaces/etc/vpnc/connect.d/write_gateway_file",
+   owner => "root",
+   mode => "0755"
+  }
+
+  file {"/etc/vpnc/post-disconnect.d/":
+   ensure => 'directory',
+  }
+
+  file {"/etc/vpnc/post-disconnect.d/delete_gateway_file":
+   ensure => "file",
+   source => "puppet:///modules/ytlaces/etc/vpnc/post-disconnect.d/delete_gateway_file",
+   owner => "root",
+   mode => "0755"
+  }
+  
+  # not using nmgui at the moment,
+  # the command line is a bit more convenient.
+  # package {"stalonetray":}
+  # package {"network-manager-applet":}
+
+  # file {"/home/tsutsumi/bin/":
+  #   ensure => 'directory',
+  #   owner => "tsutsumi"
+  # }
+  # file {"/home/tsutsumi/bin/nmgui/":
+  #   ensure => "file",
+  #   source => "puppet:///modules/ytlaces/home/bin/nmgui",
+  #   owner => "tsutsumi",
+  #   mode => "0755"
+  # }
 }
