cc4fb1f54ba908c63c3fcdb3352bc2baf10b910e
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-05-24 22:53:06 -0700

    adding nuget.

--- manifests/programming.pp
+++ manifests/programming.pp
@@ -6,4 +6,17 @@
    # package {"code":}
    package {"dotnet-runtime":}
    package {"dotnet-sdk":}
+   package {"mono":}
+   # this next part sets up nuget
+   file {"/home/tsutsumi/lib/nuget.exe":
+      ensure => "file",
+      source => "https://dist.nuget.org/win-x86-commandline/v4.7.0/nuget.exe",
+      owner => "tsutsumi",
+   }
+   file {"/home/tsutsumi/bin/nuget":
+      ensure => "file",
+      source => "puppet:///modules/ytlaces/home/bin/nuget",
+      owner => "tsutsumi",
+      mode => "0755"
+   }
 }
