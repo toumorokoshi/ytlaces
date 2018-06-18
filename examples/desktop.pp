9e95be5040aa382cb410113ebfe0e60071191ca0
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-06-17 22:35:03 -0700

    Adding legacy driver support

--- examples/desktop.pp
+++ examples/desktop.pp
@@ -1,3 +1,8 @@
 class {'::ytlaces':
   type => 'desktop'
+
+  # Using a GeForces TI 560, which is only
+  # supported by legacy drivers now.
+  package {"nvidia-390xx":}
+  package {"nvidia-390xx-utils":}
 }
