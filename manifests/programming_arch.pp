625ad12c1a72bc00a8ef9dbe6a3d3de8ed1edcee
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-02-08 21:19:01 -0800

    formatting.

--- manifests/programming_arch.pp
+++ manifests/programming_arch.pp
@@ -1,47 +1,49 @@
 class ytlaces::programming_arch(
   String $username = 'tsutsumi'
 ) {
-  package {"atom":}
-  package {"clojure":}
-  package {"docker":}
-  service {"docker":
-    ensure => "running",
-    enable => "true"
+  package {'atom':}
+  package {'clojure':}
+  package {'docker':}
+  # for python vscode symbols
+  package {'ctass':}
+  service {'docker':
+    ensure => 'running',
+    enable => true
   }
-  group {"docker":
+  group {'docker':
     members => [$username]
   }
-   package {"python":}
+   package {'python':}
    # yaourt
    # package {"code":}
-   package {"dotnet-runtime":}
-   package {"dotnet-sdk":}
-   package {"mono":}
+   package {'dotnet-runtime':}
+   package {'dotnet-sdk':}
+   package {'mono':}
    # this next part sets up nuget
-   file {"/home/$username/lib/nuget.exe":
-      ensure => "file",
-      source => "https://dist.nuget.org/win-x86-commandline/v4.7.0/nuget.exe",
-      owner => "$username",
+   file {"/home/${username}/lib/nuget.exe":
+      ensure => 'file',
+      source => 'https://dist.nuget.org/win-x86-commandline/v4.7.0/nuget.exe',
+      owner  => $username,
   }
-  file {"/home/$username/bin/nuget":
-      ensure => "file",
-      source => "puppet:///modules/ytlaces/home/bin/nuget",
-      owner => "$username",
-      mode => "0755"
+  file {"/home/${username}/bin/nuget":
+      ensure => 'file',
+      source => 'puppet:///modules/ytlaces/home/bin/nuget',
+      owner  => $username,
+      mode   => '0755'
   }
 
-  package {"gdb":}
-  file {"/home/$username/.gdbinit":
+  package {'gdb':}
+  file {"/home/${username}/.gdbinit":
     ensure => 'file',
-    source => "puppet:///modules/ytlaces/home/.gdbinit",
-    mode => '0755',
-    owner => $username,
+    source => 'puppet:///modules/ytlaces/home/.gdbinit',
+    mode   => '0755',
+    owner  => $username,
   }
 
-  file {"/home/$username/bin/flamegraph.pl":
-      ensure => "file",
-      source => "https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl",
-      owner => $username,
-      mode => "0755"
+  file {"/home/${username}/bin/flamegraph.pl":
+      ensure => 'file',
+      source => 'https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl',
+      owner  => $username,
+      mode   => '0755'
   }
 }
