3a75aee19b8467c93d5a6bdeab5bf8e61e6f8c0f
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-08-19 10:24:51 -0700

    adding intel driver configs for work.

--- manifests/terminal_config.pp
+++ manifests/terminal_config.pp
@@ -9,23 +9,9 @@ class ytlaces::terminal_config(
     owner   => $username,
   }
 
-
-   file {"/home/${username}/.rc.d/common":
-    ensure => 'file',
-    source => 'puppet:///modules/ytlaces/rc.d/common',
-    owner  => $username,
-   }
-
-   file {"/home/${username}/.rc.d/profile":
-    ensure => 'file',
-    source => 'puppet:///modules/ytlaces/rc.d/common',
-    owner  => $username,
-   }
-
-
-   file {"/home/${username}/.tmux.conf":
-    ensure => 'file',
-    source => 'puppet:///modules/ytlaces/home/.tmux.conf',
-    owner  => $username,
-   }
+  file {"/home/${username}/.tmux.conf":
+   ensure => 'file',
+   source => 'puppet:///modules/ytlaces/home/.tmux.conf',
+   owner  => $username,
+  }
 }
--- manifests/work_desktop.pp
+++ manifests/work_desktop.pp
@@ -1,19 +1,24 @@
 class ytlaces::work_desktop(
-  String $username = 'yusuket' 
+  String $username = 'yusuket'
 ) {
-  file { "/home/$username/.Xresources.d":
-    ensure => "directory",
-    owner => $username,
+  file { "/home/${username}/.Xresources.d":
+    ensure => 'directory',
+    owner  => $username,
   }
 
-  file {"/home/$username/.Xresources.d/laptop":
-    ensure => "file",
-    source => "puppet:///modules/ytlaces/.Xresources.laptop",
-    owner => $username
+  file {"/home/${username}/.Xresources.d/laptop":
+    ensure => 'file',
+    source => 'puppet:///modules/ytlaces/.Xresources.laptop',
+    owner  => $username
   }
-  file {"/home/$username/.xprofile.work":
-    ensure => "file",
-    source => "puppet:///modules/ytlaces/.xprofile.work",
-    owner => $username
+  file {"/home/${username}/.xprofile.work":
+    ensure => 'file',
+    source => 'puppet:///modules/ytlaces/.xprofile.work',
+    owner  => $username
+  }
+
+  file {'/etc/X11/xorg.conf.d/20-intel.conf':
+    ensure => 'file',
+    source => 'puppet:///modules/ytlaces/work/etc/X11/xorg.conf.d/20-intel.conf',
   }
 }
