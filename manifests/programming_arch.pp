108b4eace95f57dfca96b1b844c96cd33766ef21
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-08-21 21:29:19 -0700

    adding pyenv

--- manifests/programming_arch.pp
+++ manifests/programming_arch.pp
@@ -13,14 +13,16 @@ class ytlaces::programming_arch(
   group {'docker':
     members => [$username]
   }
-   package {'python':}
-   # yaourt
-   package {'code':}
-   package {'dotnet-runtime':}
-   package {'dotnet-sdk':}
-   package {'mono':}
-   # this next part sets up nuget
-   file {"/home/${username}/lib/nuget.exe":
+  # python
+  package {'python':}
+  package {'pyenv':}
+  # yaourt
+  package {'code':}
+  package {'dotnet-runtime':}
+  package {'dotnet-sdk':}
+  package {'mono':}
+  # this next part sets up nuget
+  file {"/home/${username}/lib/nuget.exe":
       ensure => 'file',
       source => 'https://dist.nuget.org/win-x86-commandline/v4.7.0/nuget.exe',
       owner  => $username,
