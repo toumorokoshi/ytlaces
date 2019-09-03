5c9b92d04073a9b7d58254a8dbd7416ba1c8eb3e
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2019-09-03 07:09:26 -0700

    Adding init file
    an init file offers more control around how ytlaces is initialized.
    It also enables the sourcing of multiple files.

--- manifests/terminal_config.pp
+++ manifests/terminal_config.pp
@@ -1,6 +1,12 @@
 class ytlaces::terminal_config(
   String $username = 'tsutsumi'
 ){
+  file {"/home/${username}/.ytlaces/init":
+    ensure => 'file',
+    source => 'puppet:///modules/ytlaces/init',
+    owner  => $username,
+  }
+
   file {"/home/${username}/.ytlaces/rc.d":
     ensure  => 'directory',
     recurse => true,
