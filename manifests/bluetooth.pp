2a126080095a7f84f8c440e210a02d93af63dc1f
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2022-04-21 23:33:27 -0700

    setting bluetooth always on
    when I close the lid on my laptop, I have to re-do the whole bluetooth
    sequence over again to connect to my headphones.
    
    Modifying the setting so the experience is seamless.

--- manifests/bluetooth.pp
+++ manifests/bluetooth.pp
@@ -6,4 +6,9 @@ class ytlaces::bluetooth {
     provider => 'systemd',
     enable   => true,
   }
+
+  file {'/etc/bluetooth/main.conf':
+    ensure => 'file',
+    source => 'puppet:///modules/ytlaces/etc/bluetooth/main.conf',
+  }
 }
