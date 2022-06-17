0559932ebf3f39b650c1043f7aafd2ae3d39f5fc
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2022-06-16 23:42:41 -0700

    adding pulseaudio-alsa packages
    Some applications that attempt to access alsa directly (ruffle, wine) need
    the pulseaudio-alsa package to route to pulseaudio instead.

--- manifests/audio.pp
+++ manifests/audio.pp
@@ -4,4 +4,13 @@ class ytlaces::audio {
   package {'pavucontrol':}
   # used to control media buttons
   package {'playerctl': }
+  # not clear if this is just for arch or not yet.
+  # install some pulseaudio plugins
+  # pulseaudio-alsa ensures applications that connect directly to
+  # alsa (wine, ruffle) use pulseaudio instead.
+  package {'pulseaudio-alsa':}
+  # install 32bit variant binaries for steam / 32bit app support.
+  package {'lib32-libpulse':}
+  package {'lib32-alsa-plugins':}
+
 }
