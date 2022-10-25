14659005be06c2b8471eeb83d6fbc16a376271cd
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2022-10-24 22:28:16 -0700

    add ergodox game layer, switching to pipewire
    Pipewire is the replacement for pulseaudio, so switching to that.
    
    removing some unneeded packages as part of that (bluetooth)
    
    Adding more to the ergodox game layer to make it more of an actual
    keyboard.

--- manifests/audio.pp
+++ manifests/audio.pp
@@ -1,15 +1,13 @@
 class ytlaces::audio {
   package {'alsa-utils':}
-  package {'pulseaudio':}
+  # using pipewire rather than pulseaudio,
+  # as it seems pipewire is the sucessor.
+  package {'pipewire':}
+  package {'pipewire-alsa':}
+  package {'pipewire-pulse':}
   package {'pavucontrol':}
   # used to control media buttons
   package {'playerctl': }
-  # not clear if this is just for arch or not yet.
-  # install some pulseaudio plugins
-  # pulseaudio-alsa ensures applications that connect directly to
-  # alsa (wine, ruffle) use pulseaudio instead.
-  package {'pulseaudio-alsa':}
-  # install 32bit variant binaries for steam / 32bit app support.
   package {'lib32-libpulse':}
   package {'lib32-alsa-plugins':}
 
--- manifests/bluetooth.pp
+++ manifests/bluetooth.pp
@@ -1,6 +1,5 @@
 class ytlaces::bluetooth {
   package {'blueman':}
-  package {'pulseaudio-bluetooth':}
   service { 'bluetooth.service':
     ensure   => 'running',
     provider => 'systemd',
