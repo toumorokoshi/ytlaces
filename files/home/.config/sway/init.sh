14cd4bff9e563fcac5fecbe9c6fbe9d72fc2de87
Yusuke Tsutsumi <yusuke.tsutsumi@getcruise.com>
2024-10-07 08:26:29 -0700

    removing authcli
    I start it myself anyway.

--- files/home/.config/sway/init.sh
+++ files/home/.config/sway/init.sh
@@ -13,8 +13,6 @@ systemctl --user start pipewire-media-session
 
 # start chrome next, so authcli doesn't fail.
 google-chrome --profile-directory="Profile 1"
-# finally start authcli
-/usr/bin/authcli fastpass proxy -debug > /tmp/fastpass.log &
 # add a hook for custom scripts as needed.
 if [ -f "${HOME}/.yft_custom.sh" ]; then
     . "${HOME}/.yft_custom.sh"
