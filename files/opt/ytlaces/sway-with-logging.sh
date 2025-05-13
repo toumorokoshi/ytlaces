f89003d7d0727a75af0da0a666c8169ddd4a91f1
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2025-05-12 21:27:20 -0700

    moving more files to nextgen, add fish config
    - moving home user files
    - adding config needed for fish

--- files/opt/ytlaces/sway-with-logging.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-exec > ~/var/log/sway.log
-exec sway > ~/.cache/sway.log 2>&1
--- nextgen/bin/install-base.py
+++ nextgen/bin/install-base.py
@@ -3,12 +3,18 @@
 import logging
 import os
 import shutil
+import sys
 from pathlib import Path
 
 MAIN_DIR = Path(__file__).resolve().parent.parent
 FILES_DIR = os.path.join(MAIN_DIR, "files")
+FILES_HOME_DIR = os.path.join(MAIN_DIR, "files-home")
 LOGGER = logging.getLogger("install-base")
+PPAS = {
+    "ppa:fish-shell/release-4",  # fish shell
+}
 PACKAGES = {
+    "fish",  # fish shell
     # helpful utility to visualize disk usage.
     "ncdu",
     # window manager I prefer to use
@@ -17,9 +23,18 @@ PACKAGES = {
     "fcitx5",
     "fcitx5-mozc",
 }
+# standard rust packages
+RUST_PACKAGES = {
+    "cargo-binstall",
+}
+# some packages recommend installation via cargo binstall
+RUST_BINSTALL_PACKAGES = {
+    "jj-cli",
+}
 
 
-def main():
+def main(argv=sys.argv[1:]):
+    username = argv[0]
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -34,14 +49,42 @@ def main():
         LOGGER.critical(f"Files directory {FILES_DIR} does not exist.")
         return
 
+    _add_ppas(PPAS)
     _install_packages(PACKAGES)
+    _install_and_update_rust(username)
     _recursive_copy_files(FILES_DIR, "/")
+    _recursive_copy_files(FILES_HOME_DIR, os.path.join("/home", username))
+
+
+def _add_ppas(ppas: set[str]):
+    # Add PPAs to the system
+    for ppa in ppas:
+        os.system(f"add-apt-repository -y {ppa}")
+        LOGGER.info(f"Added PPA: {ppa}")
+
+    # Update package list after adding PPAs
+    os.system("apt update")
+    LOGGER.info("Updated package list after adding PPAs")
 
 
 def _install_packages(packages: set[str]):
     # Install required packages
-    os.system(f"sudo apt -y {' '.join(packages)}")
+    os.system(f"sudo apt install -y {' '.join(packages)}")
     LOGGER.info(f"Installed packages: {', '.join(packages)}")
+
+
+def _install_and_update_rust(username: str):
+    # Install rustup if not already installed
+    # if not shutil.which("rustup"):
+    #     os.system("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh")
+    #     LOGGER.info("Installed rustup")
+
+    # Update rustup and install the latest stable version of Rust
+    _run_as_user(username, "rustup default stable")
+    # install cargo packages
+    _run_as_user(username, f"cargo install {' '.join(RUST_PACKAGES)}")
+    # install cargo binstall packages
+    _run_as_user(username, f"cargo binstall -y {' '.join(RUST_BINSTALL_PACKAGES)}")
 
 
 # recursive copy of files in the ./files directory into the root filesystem
@@ -58,6 +101,18 @@ def _recursive_copy_files(src, dst):
             target_file = os.path.join(target_dir, file)
             LOGGER.info(f"writing {target_file}")
             shutil.copy2(source_file, target_file)
+
+
+def _run_as_user(username: str, command: str):
+    """
+    Run a shell command as a specific user.
+    """
+    full_command = f'su - {username} -c "{command}"'
+    result = os.system(full_command)
+    if result != 0:
+        LOGGER.error(f"Command failed: {command}")
+    else:
+        LOGGER.info(f"Successfully ran command as {username}: {command}")
 
 
 if __name__ == "__main__":
